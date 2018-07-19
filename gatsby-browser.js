70da03e0118f3e6b1d270f753db339805203a512
Alexander Glintschert <alexander.glintschert@asideas.de>
2018-07-19 13:07:58 +0200

    OIL-208: fix scrolling and anchor issues

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,8 @@
+import { anchorate } from 'anchorate'
+exports.onRouteUpdate = () => {
+  anchorate()
+}
+
 exports.onClientEntry = () => {
   document.addEventListener("DOMContentLoaded", function (event) {
     new SmoothScroll('a[href*="#"]');
--- src/layout/header/header.js
+++ src/layout/header/header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import LinkButton from '../button/link-button';
-import ScrollableAnchor from 'react-scrollable-anchor'
 
 require('./header.scss');
 
@@ -37,49 +36,47 @@ class Header extends React.Component {
 
   render() {
     return (
-      <ScrollableAnchor id={'header'}>
-        <div id="header" className="header">
-          <div className="header-content">
-            <div className="header-logo">
-              <div className="header-logo__image">
-                <a href="/#header" className="home-link"><img src={require('./logo.svg')}/></a>
-              </div>
-              <div className="header-logo__text">
-                <a href="/#header" className="home-link">oil.js</a>
-              </div>
+      <div id="header" className="header">
+        <div className="header-content">
+          <div className="header-logo">
+            <div className="header-logo__image">
+              <a href="/#header" className="home-link"><img src={require('./logo.svg')}/></a>
             </div>
-            <div className="header-menu">
-              <MenuEntry text="Product" link="#product"/>
-              <MenuEntry text="Features" link="#features"/>
-              <MenuEntry text="Collaboration" link="#collaboration"/>
-              <MenuEntry text="Support" link="#support"/>
-              <MenuEntry text="About us" link="#aboutus"/>
-
-              <LinkButton text="Sandbox" cssClass="menu-button blue-design"/>
-              <LinkButton text="Github" cssClass="menu-button"/>
-
-              <li className='header-menu__burger-toggle' onClick={this.toggleMenu.bind(this)}>
-                <IconBurger/>
-              </li>
+            <div className="header-logo__text">
+              <a href="/#header" className="home-link">oil.js</a>
             </div>
           </div>
-          <div className="header__divider"/>
+          <div className="header-menu">
+            <MenuEntry text="Product" link="/#product"/>
+            <MenuEntry text="Features" link="/#features"/>
+            <MenuEntry text="Collaboration" link="/#collaboration"/>
+            <MenuEntry text="Support" link="/#support"/>
+            <MenuEntry text="About us" link="/#aboutus"/>
 
-          {
-            (this.state.expanded ?
-              <div className="burger-menu-content">
-                <div className="burger-menu-content__list">
-                  <MenuEntry text="Product" link="#product" onClick={this.hideMenu.bind(this)}/>
-                  <MenuEntry text="Developer" link="#developer" onClick={this.hideMenu.bind(this)}/>
-                  <MenuEntry text="Collaboration" link="#collaboration" onClick={this.hideMenu.bind(this)}/>
-                  <MenuEntry text="Support" link="#support" onClick={this.hideMenu.bind(this)}/>
-                  <MenuEntry text="About us" link="#aboutus" onClick={this.hideMenu.bind(this)}/>
-                </div>
-              </div>
-              : '')
-          }
+            <LinkButton text="Sandbox" cssClass="menu-button blue-design"/>
+            <LinkButton text="Github" cssClass="menu-button"/>
+
+            <li className='header-menu__burger-toggle' onClick={this.toggleMenu.bind(this)}>
+              <IconBurger/>
+            </li>
+          </div>
         </div>
-      </ScrollableAnchor>
+        <div className="header__divider"/>
+
+        {
+          (this.state.expanded ?
+            <div className="burger-menu-content">
+              <div className="burger-menu-content__list">
+                <MenuEntry text="Product" link="#product" onClick={this.hideMenu.bind(this)}/>
+                <MenuEntry text="Developer" link="#developer" onClick={this.hideMenu.bind(this)}/>
+                <MenuEntry text="Collaboration" link="#collaboration" onClick={this.hideMenu.bind(this)}/>
+                <MenuEntry text="Support" link="#support" onClick={this.hideMenu.bind(this)}/>
+                <MenuEntry text="About us" link="#aboutus" onClick={this.hideMenu.bind(this)}/>
+              </div>
+            </div>
+            : '')
+        }
+      </div>
     )
   }
 }
