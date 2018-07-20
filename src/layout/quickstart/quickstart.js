0312cd71492998998b43a0300ae0145909bbfeea
D. Klevinghaus <adk@codekommando.de>
2018-07-20 11:20:30 +0200

    Tiny improvements on language

--- src/layout/quickstart/quickstart.js
+++ src/layout/quickstart/quickstart.js
@@ -13,18 +13,18 @@ class Quickstart extends React.Component {
         <div className="quickstart-content section-content">
           <h6>DEVELOPER</h6>
           <h2>Quickstart</h2>
-          <p>Setting up OIL on your page is comparatively easy. Only three steps are necessary:</p>
+          <p>Setting up OIL on your page is easy and takes only three steps:</p>
           <ul className="fa-ul">
-            <li><i className="fa fa-li fa-angle-right"/> Install OIL on a host of your site.</li>
+            <li><i className="fa fa-li fa-angle-right"/> Upload the OIL files to your site.</li>
             <li><i className="fa fa-li fa-angle-right"/> Add the oil.js javascript snippet to your page.</li>
-            <li><i className="fa fa-li fa-angle-right"/> Add a configuration block to your pages DOM.</li>
+            <li><i className="fa fa-li fa-angle-right"/> Add a configuration block to your page's DOM.</li>
           </ul>
           <p>If you want a more customized appearance of the layer, you can additionally</p>
           <ul className="fa-ul">
             <li><i className="fa fa-li fa-angle-right"/> Style your layer according to your style guide.</li>
             <li><i className="fa fa-li fa-angle-right"/> Create your own language pack.</li>
           </ul>
-          <p>For details how to do this please take a look into our <a href="http://oil.axelspringer.com/docs/#quickstart" target="_blank">Opt-in Layer Quickstart Guide</a>.</p>
+          <p>For details on how to do this please consult our <a href="http://oil.axelspringer.com/docs/#quickstart" target="_blank">Opt-in Layer Quickstart Guide</a>.</p>
         </div>
       </div>
     )
