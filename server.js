063c17f3f8f73a6d6c3460784c7c92bf5303a51e
Awerkow-Schäfer, Dmitri <dmitri.awerkow@asideas.de>
2018-08-14 12:35:27 +0200

    OIL-208: enforce ssl for heroku

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ let port = process.env.PORT || 8080;
 let express = require('express');
 let compression = require('compression');
 let app = express();
+let enforce = require('express-sslify');
 
+// Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
+// a load balancer (e.g. Heroku).
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(compression());
 app.use(express.static(__dirname + '/public'));
 
