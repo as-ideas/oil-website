dca0bc400d8243a36f5da748699c5ef87554d374
Awerkow-Schäfer, Dmitri <dmitri.awerkow@asideas.de>
2018-10-04 13:48:21 +0200

    no-ticket: add redirect non-www to www #7

--- server.js
+++ server.js
@@ -6,22 +6,22 @@ let enforce = require('express-sslify');
 
 // Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
 // a load balancer (e.g. Heroku).
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
+//app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(compression());
-app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res, next) {
-  console.log('Hello iam log: ', req, res, next);
+  console.log('hello 1');
   if (req.headers.host.slice(0, 3) !== 'www') {
-    res.redirect('https://www.' + req.headers.host + req.url, 301);
+    res.redirect(301, 'https://www.' + req.headers.host + req.url);
   } else {
     next();
   }
 });
 
-// app.get('*', (req, res) => {
-//   res.sendFile(__dirname + '/public/index.html');
-// });
+app.get('/', (req, res) => {
+  console.log('hello 2');
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 app.listen(port, function () {
     console.log('server is now starting on port ', port);
