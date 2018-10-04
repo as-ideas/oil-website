52d992beceabf0cc5476009f110a8741761bb1b2
Awerkow-Schäfer, Dmitri <dmitri.awerkow@asideas.de>
2018-10-04 13:59:39 +0200

    no-ticket: rollback

--- server.js
+++ server.js
@@ -6,23 +6,10 @@ let enforce = require('express-sslify');
 
 // Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
 // a load balancer (e.g. Heroku).
-//app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(compression());
-
-app.get('*', function(req, res, next) {
-  console.log('hello 1');
-  if (req.headers.host.slice(0, 3) !== 'www') {
-    res.redirect(301, 'https://www.' + req.headers.host + req.url);
-  } else {
-    next();
-  }
-});
-
-app.get('/', (req, res) => {
-  console.log('hello 2');
-  res.sendFile(__dirname + '/public/index.html');
-});
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, function () {
-    console.log('server is now starting on port ', port);
+  console.log('server is now starting on port ', port);
 });
