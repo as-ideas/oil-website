db764e4600730d4ebe4e4ce70b3e25355a011daf
Sebastian Waschnick <sebastian@waschnick.de>
2019-02-28 09:53:29 +0100

    Adding cors

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ let express = require('express');
 let compression = require('compression');
 let app = express();
 let enforce = require('express-sslify');
+const cors = require('cors');
 
 // Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
 // a load balancer (e.g. Heroku).
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
+// app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(compression());
-app.use(express.static(__dirname + '/public'));
+app.use(cors(), express.static(__dirname + '/public'));
 
 app.listen(port, function () {
   console.log('server is now starting on port ', port);
